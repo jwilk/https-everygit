6b18251fe0957043bd61009ae1fa60fb098f9258
Jakub Wilk <jwilk@jwilk.net>
2024-03-07 16:58:09 +0100

    Add git.breakpoint.cc.

---
+++ data/git.breakpoint.cc
@@ -0,0 +1,8 @@
+[rules]
+git://git.breakpoint.cc/ = https://git.breakpoint.cc/git/
+
+[tests]
+git://git.breakpoint.cc/fw/tcprdr.git = https://git.breakpoint.cc/git/fw/tcprdr.git
+http://git.breakpoint.cc/git/fw/tcprdr.git = https://git.breakpoint.cc/git/fw/tcprdr.git
+
+# vim:ft=dosini

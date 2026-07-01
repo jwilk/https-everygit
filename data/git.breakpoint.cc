b68a1466a9dd62d94aec5316612b4a687c3ae527
Jakub Wilk <jwilk@jwilk.net>
2026-07-01 14:22:06 +0200

    Mark git.breakpoint.cc as offline.

--- data/git.breakpoint.cc
+++ data/git.breakpoint.cc
@@ -2,7 +2,7 @@
 git://git.breakpoint.cc/ = https://git.breakpoint.cc/git/
 
 [tests]
-git://git.breakpoint.cc/fw/tcprdr.git = https://git.breakpoint.cc/git/fw/tcprdr.git
-http://git.breakpoint.cc/git/fw/tcprdr.git = https://git.breakpoint.cc/git/fw/tcprdr.git
+git://git.breakpoint.cc/fw/tcprdr.git OFFLINE = https://git.breakpoint.cc/git/fw/tcprdr.git OFFLINE
+http://git.breakpoint.cc/git/fw/tcprdr.git OFFLINE = https://git.breakpoint.cc/git/fw/tcprdr.git OFFLINE
 
 # vim:ft=dosini
